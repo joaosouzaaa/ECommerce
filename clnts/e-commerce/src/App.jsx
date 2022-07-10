dbc4bbf3d902761e4fdb8170b913659c96b85b74
castro <igor.castro@xbits.com.br>
2022-07-10 16:25:37 -0300

    ica alterado para create-react-app

---
+++ clnts/e-commerce/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
+import About from "./pages/About/About";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Account from "./pages/Account/Account";
+import SignIn from "./pages/SignIn/SignIn";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <NavBarComponent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ clnts/e-commerce/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Componente home</h1>
+    </div>
+  )
+}
+
+export default Home
