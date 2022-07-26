1d7b5a6521dbff23d91531d15ae7b1b008fb8919
IgorSeven <igorsevenn@gmail.com>
2022-07-25 23:32:34 -0300

    Staged changes

--- apisrc/ECommerce/ECommerce.TestShoppingCart/UnitTest/Validation/ProductValidationTest.cs
+++ apisrc/ECommerce/ECommerce.TestShoppingCart/UnitTest/Validation/ProductValidationTest.cs
@@ -5,11 +5,10 @@ namespace ECommerce.TestShoppingCart.UnitTest.Validation
 {
     public class ProductValidationTest
     {
-<<<<<<< HEAD
+
         private readonly ProductValidation _validate;
-=======
-        ProductValidation _validate;
->>>>>>> 5b231b8fb15a9f8ebac1753666011663ed13f599
+
+
 
         public ProductValidationTest()
         {
@@ -22,62 +21,48 @@ namespace ECommerce.TestShoppingCart.UnitTest.Validation
         {
             var product = ProductBuilder.NewObject().DomainBuilder();
 
-<<<<<<< HEAD
+
             var result = await _validate.ValidationAsync(product);
 
             Assert.True(result.Valid);
-=======
+
             var response = await _validate.ValidateAsync(product);
 
             Assert.True(response.IsValid);
->>>>>>> 5b231b8fb15a9f8ebac1753666011663ed13f599
+
         }
 
         [Theory(DisplayName = "Product Invalid")]
         [Trait("Category", "Product Validation")]
         [InlineData("llllllllllllllllllllllCinquentalllllllllllllllllllllllllllllllllllllllllllllll")]
         [InlineData("T")]
-<<<<<<< HEAD
-        [InlineData("")]
-        public async Task ProductValidation_InvalidName_ReturnInvalid(string name)
-=======
         public async Task ProductValidationProperties_InvalidName_ReturnsFalse(string name)
->>>>>>> 5b231b8fb15a9f8ebac1753666011663ed13f599
+
         {
             var product = ProductBuilder.NewObject().WithName(name).DomainBuilder();
 
-<<<<<<< HEAD
-            var result = await _validate.ValidationAsync(Product);
 
-            Assert.False(!result.Valid);
-=======
-            var response = await _validate.ValidateAsync(product);
 
-            Assert.False(response.IsValid);
->>>>>>> 5b231b8fb15a9f8ebac1753666011663ed13f599
+            var response = await _validate.ValidationAsync(product);
+
+            Assert.False(response.Valid);
+
         }
 
         [Theory]
         [InlineData("cento e cinquentalllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")]
         [InlineData("ll")]
         [InlineData("T")]
-<<<<<<< HEAD
-        [InlineData("")]
-        public async Task ProductValidation_InvalidDescription_ReturnInvalid(string description)
-=======
         public async Task ProductValidationProperties_InvalidDescription_ReturnsFalse(string description)
->>>>>>> 5b231b8fb15a9f8ebac1753666011663ed13f599
+
         {
             var product = ProductBuilder.NewObject().WithDescription(description).DomainBuilder();
 
-<<<<<<< HEAD
-            var response = await _validate.ValidationAsync(Product);
+            var response = await _validate.ValidationAsync(product);
 
-            Assert.False(!response.Valid);
-=======
-            var response = await _validate.ValidateAsync(product);
 
-            Assert.False(response.IsValid);
+
+            Assert.False(response.Valid);
         }
 
         [Theory]
@@ -114,7 +99,7 @@ namespace ECommerce.TestShoppingCart.UnitTest.Validation
             var response = await _validate.ValidateAsync(product);
 
             Assert.False(response.IsValid);
->>>>>>> 5b231b8fb15a9f8ebac1753666011663ed13f599
+
         }
     }
 }
